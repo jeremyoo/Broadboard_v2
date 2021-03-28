c496c81bfbf01d285c73fdf950486bfb25d12d49
jeremyoo <jyyoo824@gmail.com>
2021-03-29 00:04:02 +0100

    Add TOC on PostViewer component

--- blog-frontend/src/components/post/PostViewer.js
+++ blog-frontend/src/components/post/PostViewer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
 import Tags from '../common/Tags';
@@ -59,27 +59,103 @@ const PostContent = styled.div`
   overflow-wrap: break-word;
 `;
 
+const PostTocBlock = styled.div`
+  ${props => props.scrolled && css`
+    .postToc {
+      position: fixed;
+      left: calc(50vw + 384px);
+    }
+  `}
+  position: relative;
+  margin-top: 1rem;
+  .postTocPosition {
+    position: absolute;
+    left: 100%;
+  }
+  .postToc {
+    margin-left: 4rem;
+    padding-left: 1rem;
+    border-left: 3px solid ${palette.gray[2]};
+    width: 250px;
+    div {
+      margin: 0.4rem 0;
+      cursor: pointer;
+      &:hover {
+        font-weight: bold;
+      }
+    }
+    .active {
+      font-weight: bold;
+    }
+  }
+
+`;
+
 const PostViewer = ({ post, error, loading, actionButtons }) => {
 
   const contentRef = useRef();
+  const scrollRef = useRef();
+  const tocElement = useRef();
+
   const [ toc, setToc ] = useState([]);
+  const [ scrolled, setScrolled ] = useState(false);
+  const [ itemTop, setItemTop ] = useState(0);
 
   useEffect(() => {
-    console.log("viewerInner");
     if (!loading && post) {
       const content = contentRef.current.children;
       const arry = Array.from(content)
       arry.forEach(a => {
-        if (a.localName === 'ul') {
+        if (a.localName === 'h1' || a.localName === 'h2' ) {
           setToc(arr => [...arr, {
             tag: a.localName,
             text: a.innerText,
-            top: a.offsetTop,
+            top: a.offsetTop - 65,
           }])
         }
       });
+      setItemTop(scrollRef.current.offsetTop - 250);
     }
   }, [loading])
+
+  const tocFix = useCallback(() => {
+    if (document.documentElement.scrollTop >= 250) {
+      return setScrolled(true);
+    };
+    setScrolled(false);
+  })
+
+  useEffect(() => {
+    window.addEventListener("scroll", tocFix, true);
+    return () => {
+      window.removeEventListener("scroll", tocFix, true);
+    }
+  }, [loading, tocFix])
+
+  const tocBold = useCallback(() => {
+    if (toc.length !== 0) {
+      toc.map((a, index) => {
+        const tocRef =  tocElement.current;
+        const currentScroll = document.documentElement.scrollTop;
+        const clientHeight = document.documentElement.clientHeight;
+        const scrollHeight = document.documentElement.scrollHeight;
+        const scrolling = currentScroll + (clientHeight * 2/5);
+        const next = toc[index + 1] === undefined ? scrollHeight : toc[index + 1].top;
+        if (a.top < scrolling && scrolling < next) {
+          tocRef.children[index].className = "active"
+        } else {
+          tocRef.children[index].className = ""
+        }
+      })
+    }
+  });
+
+  useEffect(() => {
+    window.addEventListener("scroll", tocBold, true);
+    return () => {
+      window.removeEventListener("scroll", tocBold, true);
+    }
+  }, [loading, scrolled])
 
   if (error) {
     if (error.response && error.response.status === 404) {
@@ -110,14 +186,25 @@ const PostViewer = ({ post, error, loading, actionButtons }) => {
         <Tags className='tags' tags={tags} viewerTags={viewerTags} />
       </PostHead>
       {actionButtons}
+      <PostTocBlock scrolled={scrolled} ref={scrollRef}>
+        <div className="postTocPosition">
+          <div className="postToc" ref={tocElement} style={{top: `${itemTop}px`}}>
+          {toc.map((a, index) =>
+            <div key={index} yoyyo={a.top}
+            onClick={() => window.scrollTo({
+              top: a.top,
+              left: 0,
+              behavior: 'smooth'
+            })
+          }
+            >
+              {a.text}
+            </div>
+          )}
+          </div>
+        </div>
+      </PostTocBlock>
       <PostContent ref={contentRef} dangerouslySetInnerHTML={{ __html: body }} />
-      {/* {toc.map((a) => 
-        <div key={a.top} onClick={() => window.scrollTo({
-          top: a.top,
-          left: 0,
-          behavior: 'smooth'
-        })}>{a.text}</div>
-      )} */}
     </PostViewerBlock>
   );
 };
